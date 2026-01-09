cc90ab99a1191ddf9e8c5c4ec644158d4d077c2d
ameliagmz <amelia.ameliagmg@gmail.com>
2026-01-09 11:02:27 +0100

    update main

--- main.py
+++ main.py
@@ -20,56 +20,39 @@ model, _, preprocess = open_clip.create_model_and_transforms(BACKBONE, pretraine
 model.eval()
 model = model.to(device)
 
-# start_time = time.time()
+start_time = time.time()
 
 # GENERATE ATTENTIONS
-# get_attn(model, preprocess, IMGS_FOLDER, ATTN_DIR, MAX_IMAGES) # get attentions of last 4 layers
+get_attn(model, preprocess, IMGS_FOLDER, ATTN_DIR, MAX_IMAGES) # get attentions of last 4 layers
 
 # GENERATE AVG_ATTENTIONS
-# get_avg_attns_gender(ATTN_DIR, AVG_ATTN_DIR)
-# get_avg_attns_ethnicity_ff(ATTN_DIR, AVG_ATTN_DIR)
+get_avg_attns_gender(ATTN_DIR, AVG_ATTN_DIR)
+get_avg_attns_ethnicity_ff(ATTN_DIR, AVG_ATTN_DIR)
 
 # HEADS TO EVALUATE
 layers = ['layer9_attn', 'layer10_attn', 'layer11_attn', 'layer12_attn']
 heads = [1,2,3,4,5,6,7,8,9,10,11,12]
 
-# head_classifier = SimpleMLP(768).to(device)
+head_classifier = SimpleMLP(768).to(device)
 
-# best_heads_dict, all_dfs = get_best_heads(head_classifier, layers, heads, ATTN_DIR, AP_THRESHOLD)
+best_heads_dict, all_dfs = get_best_heads(head_classifier, layers, heads, ATTN_DIR, AP_THRESHOLD)
 
-# end_time = time.time()
-# elapsed_time = end_time - start_time
-# print(f"Elapsed time: {elapsed_time:.5f} seconds")
+end_time = time.time()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.5f} seconds")
 
-# print(all_dfs[-1]) #layer 12
-
-best_heads_dict = {12:[1,2,3,4]}
 
 for l, best_heads in best_heads_dict.items():
 
-    if l==12:
+    # INTERVENTION
+    new_attn_exp, heads_exp, layers_exp, interventions_exp = generate_attention_experiments(l, best_heads, AVG_ATTN_DIR)
 
-        print(best_heads)
+    # EVALUATION
+    SISPI_eval(new_attn_exp, heads_exp, layers_exp, interventions_exp, SISPI_PATH, EXP_DIR)
 
-        # # INTERVENTION
-        # new_attn_exp, heads_exp, layers_exp, interventions_exp = generate_attention_experiments(l, best_heads, AVG_ATTN_DIR)
+    interv = (new_attn_exp, heads_exp, layers_exp, interventions_exp)
+    backbone = re.sub(r'-(\d+)$', r'/\1', BACKBONE)
 
-        # # EVALUATION
-        # # SISPI_eval(new_attn_exp, heads_exp, layers_exp, interventions_exp, SISPI_PATH, EXP_DIR)
+    eval_zhang(device=device, model_type=MODEL_TYPE, test_attribute=TEST_ATTRIBUTE, backbone=backbone, EXP_DIR=EXP_DIR, interv=interv)
 
-        whitel12_h2 = torch.load(os.path.join(AVG_ATTN_DIR, "White_l12_h2.pt"), weights_only=True)
-        whitel12_h7 = torch.load(os.path.join(AVG_ATTN_DIR, "White_l12_h7.pt"), weights_only=True)
-        whitel12_h11 = torch.load(os.path.join(AVG_ATTN_DIR, "White_l12_h11.pt"), weights_only=True)
-
-
-        new_attn_exp = [[whitel12_h2, whitel12_h7, whitel12_h11]]
-        heads_exp = [[2,7,11]]
-        layers_exp = [12]
-        interventions_exp = ['white']
-
-        interv = (new_attn_exp, heads_exp, layers_exp, interventions_exp)
-        backbone = re.sub(r'-(\d+)$', r'/\1', BACKBONE)
-
-        # # eval_zhang(device=device, model_type=MODEL_TYPE, test_attribute=TEST_ATTRIBUTE, backbone=backbone, EXP_DIR=EXP_DIR, interv=interv)
-
-        eval_So_B_IT(model, preprocess, MODEL_TYPE, backbone, TEST_ATTRIBUTE, interv, FF_CROP)
+    eval_So_B_IT(model, preprocess, MODEL_TYPE, backbone, TEST_ATTRIBUTE, interv, FF_CROP)
